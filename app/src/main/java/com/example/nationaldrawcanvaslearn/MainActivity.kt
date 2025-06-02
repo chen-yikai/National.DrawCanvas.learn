0065395478adb28879673f1c704e89d606308b61
chenelias <eliaschen.dev@gmail.com>
2025-06-02 20:29:15 +0800

    add canvas

--- app/src/main/java/com/example/nationaldrawcanvaslearn/MainActivity.kt
+++ app/src/main/java/com/example/nationaldrawcanvaslearn/MainActivity.kt
@@ -4,13 +4,54 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.animation.animateContentSize
+import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.gestures.detectDragGestures
+import androidx.compose.foundation.gestures.detectTapGestures
+import androidx.compose.foundation.gestures.rememberDraggableState
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.systemBarsPadding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Delete
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Slider
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableFloatStateOf
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.StrokeCap
+import androidx.compose.ui.input.pointer.pointerInput
+import androidx.compose.ui.unit.dp
 import com.example.nationaldrawcanvaslearn.ui.theme.NationalDrawCanvaslearnTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,29 +60,87 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             NationalDrawCanvaslearnTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                DrawCanvas()
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
 
-@Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    NationalDrawCanvaslearnTheme {
-        Greeting("Android")
+fun DrawCanvas() {
+    val lines = remember { mutableStateListOf<Line>() }
+    val colors = listOf(Color.Black, Color(0xff685FFF), Color(0xffFFD886), Color(0xffFF5185))
+    var selectedColor by remember { mutableIntStateOf(0) }
+    var selectedStroke by remember { mutableFloatStateOf(10f) }
+    Column(Modifier.fillMaxSize()) {
+        Canvas(
+            modifier = Modifier
+                .fillMaxWidth()
+                .weight(1f)
+                .pointerInput(true) {
+                    detectDragGestures { change, dragAmount ->
+                        change.consume()
+                        val line = Line(
+                            start = change.position - dragAmount,
+                            end = change.position,
+                            color = colors.get(selectedColor), strokeWidth = selectedStroke
+                        )
+                        lines.add(line)
+                    }
+                },
+        ) {
+            lines.forEach { line ->
+                drawLine(
+                    color = line.color,
+                    start = line.start,
+                    end = line.end,
+                    line.strokeWidth,
+                    cap = StrokeCap.Round
+                )
+            }
+        }
+        LazyRow(
+            Modifier
+                .fillMaxWidth()
+                .height(130.dp)
+                .systemBarsPadding(),
+            verticalAlignment = Alignment.CenterVertically,
+            contentPadding = PaddingValues(horizontal = 10.dp)
+        ) {
+            itemsIndexed(colors) { index, item ->
+                Box(
+                    modifier = Modifier
+                        .padding(horizontal = 10.dp)
+                        .clip(RoundedCornerShape(10.dp))
+                        .background(item)
+                        .clickable {
+                            selectedColor = index
+                        }
+                        .animateContentSize()
+                        .size(if (selectedColor == index) 65.dp else 55.dp))
+            }
+            item {
+                IconButton(onClick = {
+                    lines.clear()
+                }) { Icon(Icons.Default.Delete, contentDescription = "") }
+            }
+            item {
+                Slider(
+                    value = selectedStroke,
+                    onValueChange = { selectedStroke = it },
+                    modifier = Modifier.width(200.dp),
+                    valueRange = 5f..50f
+                )
+                Spacer(modifier = Modifier.width(20.dp))
+            }
+        }
     }
 }
+
+data class Line(
+    val start: Offset,
+    val end: Offset,
+    val color: Color = Color.Black,
+    val strokeWidth: Float = 20f
+)
